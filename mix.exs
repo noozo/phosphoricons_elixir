795ad38e74e381d1d7786bba8b24df9559995ca7
Pedro Assunção <assuncas@gmail.com>
2024-02-21 22:37:20 +0000

    Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PhosphoriconsElixir.MixProject do
     [
       app: :phosphoricons,
       version: "0.2.5",
-      elixir: "~> 1.14",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       name: "Phosphoricons",
