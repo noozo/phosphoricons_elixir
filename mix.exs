4bc1ca5878523e4411203ebedfc09585e8633359
Pedro Assunção <assuncas@gmail.com>
2026-05-04 14:52:03 +0100

    Bumping version to solve hexpm metadata.config file size limit

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PhosphoriconsElixir.MixProject do
   def project do
     [
       app: :phosphoricons,
-      version: "0.3.2",
+      version: "0.3.3",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -40,7 +40,8 @@ defmodule PhosphoriconsElixir.MixProject do
   defp package do
     [
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/noozo/phosphoricons_elixir"}
+      links: %{"GitHub" => "https://github.com/noozo/phosphoricons_elixir"},
+      files: ~w(lib .formatter.exs mix.exs README* CHANGELOG* LICENSE*)
     ]
   end
 
